857b5ebc5f4f4edc4bc44413695e57e26f8b39a1
Şeyhmus OK <91192631+iamseyhmus7@users.noreply.github.com>
2026-04-06 12:28:26 +0300

    Code Build , pipeline and CloudFormationTemplate text

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Merhabaaaaaaa Şeyhmusssssss! Bu Python uygulaması Docker içinden çalışıyor! 🚀"
+    return "Merhabaaaaaaa Şeyhmusssssss! AWS CodeBuild-CodePipeline ve CloudFormation Template ile ilgili testleri gerçekleştiriyoruz!! 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)   
